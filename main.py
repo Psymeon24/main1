3973d79a9f9850b6ad06cbd917124e9a636a5e08
Psymeon24 <105394189+Psymeon24@users.noreply.github.com>
2022-05-27 17:59:35 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+def find_anagrams():
+    str1 = listen
+    str2 = silent
+    if len(str1) == len(str2):
+        print("The strings are not anagrams.")
+    else:
+        print("The strings are anagrams.")
+        return true
